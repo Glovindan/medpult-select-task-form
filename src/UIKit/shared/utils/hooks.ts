a4840da825b8b4f4efa9235a5777e709bd9cb246
Назаров Антон <nazarov@elewise.local>
2024-06-06 12:11:50 +0300

    feat: Debounce

--- src/UIKit/shared/utils/hooks.ts
+++ src/UIKit/shared/utils/hooks.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-export default function useDebounce(value, delay) {
+export default function useDebounce<ValueType = any>(value: ValueType, delay: number): ValueType {
 	// Состояние и сеттер для отложенного значения
 	const [debouncedValue, setDebouncedValue] = useState(value)
 
